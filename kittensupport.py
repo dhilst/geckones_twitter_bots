97c48f093421fc9207ddb9750ba2b8756864ca98
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-06-07 01:06:14 -0300

    Place phrases on images

--- kittensupport.py
+++ kittensupport.py
@@ -3,25 +3,12 @@ import shutil
 import tempfile
 import os
 import requests
+from io import StringIO
 from random import randint, choice
 from time import sleep
 from bs4 import BeautifulSoup
 
 from tweepy import API, OAuthHandler, TweepError  # type: ignore
-
-
-def get_random_message():
-    p = randint(1,20)
-    page = requests.get(f"https://www.pensador.com/frases/{p}/")
-    if page.status_code != 200:
-        return
-    soup = BeautifulSoup(page.content, 'html.parser')
-    phrase = choice(soup.find_all('p', class_='frase'))
-    author = phrase.parent.find('span', class_='autor').find('a')
-    phrase = f'{phrase.get_text()} - {author.get_text()}'
-    return phrase
-
-
 
 def create_api() -> API:
     key = os.environ["ACCESS_KEY"]
@@ -43,10 +30,37 @@ def create_api() -> API:
     )
     return api
 
+def format_phrase(p):
+    words = p.split()
+    s = StringIO()
+    s2 = StringIO()
+    l = 0
+    for w in words:
+        s.write(w)
+        s.write(" ")
+        if len(s.getvalue()) > 32:
+            s.write("\n")
+            s2.write(s.getvalue())
+            s = StringIO()
+    if s2.getvalue().endswith("\n"):
+        s2 = StringIO(s2.getvalue()[0:-1])
+    return s2.getvalue()
+
+def get_random_message():
+    p = randint(1,20)
+    page = requests.get(f"https://www.pensador.com/frases/{p}/")
+    if page.status_code != 200:
+        return
+    soup = BeautifulSoup(page.content, 'html.parser')
+    phrase = choice(soup.find_all('p', class_='frase'))
+    author = phrase.parent.find('span', class_='autor').find('a')
+    phrase = format_phrase(phrase.get_text())
+    phrase = f'{phrase}\n\n{author.get_text()}'
+    return phrase
 
 def download_random_kitten_image():
-    "Returns the file name, user should delete it after using"
-    res = requests.get(f"https://cataas.com/cat?width=600", stream=True)
+    p = get_random_message()
+    res = requests.get(f"https://cataas.com/cat/says/{p}?width=600", stream=True)
     if res.status_code != 200:
         raise RuntimeError("placekitten request failed")
 
@@ -55,22 +69,22 @@ def download_random_kitten_image():
         shutil.copyfileobj(res.raw, f)
     return f.name
 
-
 def tweet_random_kitten(api, img_path, status=None, reply=None):
     status = api.update_with_media(
         img_path, status=status, in_reply_to_status_id=getattr(reply, 'id', None),
     )
-    return f"https://twitter.com/kiitenstatus/status/{status.id}"
+    return status
 
 def main() -> None:
     api = create_api()
+    me = api.me().screen_name
+    url = "https://twitter.com/{}/status/{{}}".format(me)
     while True:
         path = None
         try:
-            text = get_random_message()
             path = download_random_kitten_image()
-            url = tweet_random_kitten(api, path, status=text)
-            print(url)
+            status = tweet_random_kitten(api, path)
+            print(url.format(status.id))
         except TweepError as e:
             print(f"TweepError {e}")
         except RuntimeError:
