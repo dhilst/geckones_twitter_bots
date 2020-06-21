3c77a18afa9a15cb5e0a41cc9fd1220993b74b34
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-06-21 10:32:28 -0300

    Fix broken kiitensupport

--- kittensupport.py
+++ kittensupport.py
@@ -1,101 +1,92 @@
+import json
 import asyncio
 import sys
 import shutil
 import tempfile
 import os
-import requests
+import aiohttp
+
 from io import StringIO
 from random import randint, choice
 from time import sleep
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # type: ignore
 
 from tweepy import API, OAuthHandler, TweepError  # type: ignore
 
-def create_api() -> API:
-    key = os.environ["ACCESS_KEY"]
-    secret = os.environ["ACCESS_SECRET"]
-    access_token = os.environ["ACCESS_TOKEN"]
-    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
-    try:
-        auth = OAuthHandler(key, secret)
-    except TweepError:
-        sys.exit(-1)
-    auth.set_access_token(access_token, access_token_secret)
-    api = API(
-        auth,
-        retry_count=3,
-        retry_delay=10,
-        wait_on_rate_limit_notify=True,
-        wait_on_rate_limit=True,
-        compression=True,
-    )
-    return api
+import utils
+import mememaker
 
-def format_phrase(p):
-    words = p.split()
-    s = StringIO()
-    s2 = StringIO()
-    l = 0
-    for w in words:
-        s.write(w)
-        s.write(" ")
-        if len(s.getvalue()) > 32:
-            s.write("\n")
-            s2.write(s.getvalue())
-            s = StringIO()
-    s2.write(s.getvalue())
-    if s2.getvalue().endswith("\n"):
-        s2 = StringIO(s2.getvalue()[0:-1])
-    return s2.getvalue()
 
-def get_random_message():
-    p = randint(1,20)
-    page = requests.get(f"https://www.pensador.com/frases/{p}/")
-    if page.status_code != 200:
-        return
-    soup = BeautifulSoup(page.content, 'html.parser')
-    phrase = choice(soup.find_all('p', class_='frase'))
-    author = phrase.parent.find('span', class_='autor').find('a')
-    phrase = format_phrase(phrase.get_text())
-    phrase = f'{phrase}\n\n{author.get_text()}'
-    return phrase
+async def create_api() -> API:
+    key = os.environ["KIITENSUPORT_ACCESS_KEY"]
+    secret = os.environ["KIITENSUPORT_ACCESS_SECRET"]
+    access_token = os.environ["KIITENSUPORT_ACCESS_TOKEN"]
+    access_token_secret = os.environ["KIITENSUPORT_ACCESS_TOKEN_SECRET"]
+    return await utils.create_twitter(key, secret, access_token, access_token_secret)
 
-def download_random_kitten_image():
-    p = get_random_message()
-    res = requests.get(f"https://cataas.com/cat/says/{p}?width=600", stream=True)
-    if res.status_code != 200:
-        raise RuntimeError("placekitten request failed")
 
-    res.raw.decode_content = True
-    with tempfile.NamedTemporaryFile(suffix=".jpeg", delete=False) as f:
-        shutil.copyfileobj(res.raw, f)
-    return f.name
+async def get_random_message():
+    p = randint(1, 20)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://www.pensador.com/frases/{p}/") as resp:
+            if resp.status != 200:
+                return
+            content = await resp.text()
+            soup = BeautifulSoup(content, "html.parser")
+            phrase = choice(soup.find_all("p", class_="frase"))
+            if not phrase:
+                return
+            author = phrase.parent.find("span", class_="autor").find("a")
+            if not author:
+                return
+            phrase = f"“{phrase.get_text()}” — {author.get_text()}"
+            return phrase
 
-def tweet_random_kitten(api, img_path, status=None, reply=None):
-    status = api.update_with_media(
-        img_path, status=status, in_reply_to_status_id=getattr(reply, 'id', None),
-    )
-    return status
+
+async def get_random_kitten_image_url():
+    key = os.environ["KIITENSUPORT_THECATAPI_KEY"]
+    url = "https://api.thecatapi.com/v1/images/search?api_key={key}&limit=1"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status != 200:
+                return
+            data = await resp.json()
+            if not isinstance(data, (list,)) or len(data) == 0:
+                return
+            return data[0].get("url")
+
 
 async def main() -> None:
-    print("kiitensupport bot started")
-    api = create_api()
-    me = api.me().screen_name
-    url = "https://twitter.com/{}/status/{{}}".format(me)
+    utils.log.info("kiitensupport bot started")
+    api = await create_api()
     while True:
-        path = None
+        if not "DRYRUN" in os.environ:
+            await asyncio.sleep(utils.next_hour(19).seconds)
+        else:
+            await asyncio.sleep(1)
         try:
-            path = download_random_kitten_image()
-            status = tweet_random_kitten(api, path)
-            print(url.format(status.id))
+            phrase = await get_random_message()
+            if not phrase:
+                continue
+
+            kitten_url = await get_random_kitten_image_url()
+            if not kitten_url:
+                continue
+
+            async with utils.download_image(kitten_url) as path:
+                new_image_path = await mememaker.create_meme_tempfile(path, phrase)
+                if not new_image_path:
+                    continue
+
+            if not "DRYRUNx" in os.environ:
+                status = await api.update_with_media(new_image_path)
+                utils.log.info("Posted %s", await utils.get_tweet_url(api, status))
+            else:
+                utils.log.debug("DRYRUN, skipping")
         except TweepError as e:
-            print(f"TweepError {e}")
-        except RuntimeError:
-            printf("Cat as a service unavailable")
-        finally:
-            if path:
-                os.remove(path)
-        await asyncio.sleep(3600)
+            utils.log.error(f"TweepError %s", e)
+        except Exception as e:
+            utils.log.error("Uncaught exception %s", e)
 
 
 if __name__ == "__main__":
