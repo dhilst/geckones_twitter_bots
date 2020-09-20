96e8d7db23564eb3b6af91c53a332eb85552aeed
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-09-20 11:38:45 -0300

    deactive alienguy

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
 
     loop.create_task(kittensupport.main())
     loop.create_task(memesbr.main())
-    loop.create_task(aliens.main())
+    #loop.create_task(aliens.main())
     loop.create_task(mememize.main())
     loop.run_forever()
